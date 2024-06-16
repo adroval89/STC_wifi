777cdc8d76e69aa75efa41ba9c24f86b8057aef1
Adrian <adrian@192.168.1.90>
2024-06-15 22:17:59 -0300

    15-6

--- STC_wifi/STC_wifi.ino
+++ STC_wifi/STC_wifi.ino
@@ -16,7 +16,7 @@
 
 
 // variables
-struct switches{
+struct switches {
   bool cooling;
   bool heating;
   float delta;
@@ -35,58 +35,45 @@ bool led_heat;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-struct switches sw1 = {false,false,0.5,10,3,0};
+struct switches sw1 = {false, false, 0.5, 10, 3, 0};
+struct switches sw2 = {false, false, 0.5, 10, 3, 0};
 
 
 void setup() {
   // put your setup code here, to run once:
 
-pinMode(pincooling, OUTPUT);
-pinMode(pinheating, OUTPUT);
-pinMode(pinbutton_setup, INPUT);
-pinMode(pinbutton_up, INPUT);
-pinMode(pinbutton_down, INPUT);
-pinMode(pinled_on, OUTPUT);
-pinMode(pinled_cool, OUTPUT);
-pinMode(pinled_heat, OUTPUT);
-sensors.begin();
+  pinMode(pincooling, OUTPUT);
+  pinMode(pinheating, OUTPUT);
+  pinMode(pinbutton_setup, INPUT);
+  pinMode(pinbutton_up, INPUT);
+  pinMode(pinbutton_down, INPUT);
+  pinMode(pinled_on, OUTPUT);
+  pinMode(pinled_cool, OUTPUT);
+  pinMode(pinled_heat, OUTPUT);
+  sensors.begin();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-sensors.requestTemperatures(); // for the first ubidots status check and data send
-sw1.probe = sensors.getTempCByIndex(1);
+  sensors.requestTemperatures(); // for the first ubidots status check and data send
+  sw1.probe = sensors.getTempCByIndex(0);
+  sw2.probe = sensors.getTempCByIndex(1);
+  temperature_control(sw1);
+  temperature_control(sw2);
 
-if (!(sw1.heating&&sw1.cooling)){
-  sw1.heating = check_heat_on(sw1.probe,sw1.set,sw1.delta);
-  sw1.cooling = check_cool_on(sw1.probe,sw1.set,sw1.delta);
 }
-if (heating){
-  sw1.heating = check_heat_off(sw1.probe,sw1.set);
+
+void temperature_control(switches &sw) {
+  if (!(sw.heating && sw.cooling)) {
+    (sw.probe < (sw.set - sw.delta)) ? sw.heating = true : sw.heating = false;
+    (sw.probe > (sw.set + sw.delta)) ? sw.cooling = true : sw.cooling = false;
   }
-if (cooling){
-    sw1.cooling = check_cool_off(sw1.probe,sw1.set);
-}
-}
-
-bool check_heat_on(probe, set, delta){
-  (probe < (set + delta)) ? heating = true : heating = false;
-  return heating;
+  if (sw1.heating) {
+    (sw.probe >= sw.set) ? sw.heating = false : sw.heating = true;
   }
-  
-bool check_cool_on(probe,set,delta){
-  (probe < (set + delta)) ? cooling = true : cooling = false;
-  return cooling;
-}
-
-
-bool check_heat_off(probe,set){
-  probe >= set) ? heating = false : heating = true;
-      return heating;
+  if (sw1.cooling) {
+    (sw.probe <= sw.set) ? sw.cooling = false : sw.cooling = true;
   }
-bool check_cool_off(probe,set){
-    probe <= set ? cooling = false : cooling = true;
-    return cooling;
-    }
+}
