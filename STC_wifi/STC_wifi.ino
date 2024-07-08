edc6acd8188a7aaec2924f14b430a9d921c949e2
Adrian <adrian@192.168.1.5>
2024-07-07 23:34:48 -0300

    relays verified

--- STC_wifi/STC_wifi.ino
+++ STC_wifi/STC_wifi.ino
@@ -1,19 +1,26 @@
 #include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
 
-#define pincooling 1
-#define pinheating 3
+#define BLYNK_TEMPLATE_NAME "STC WIFI"
+#define BLYNK_AUTH_TOKEN "dYYjBXsLn7ogY8lDS0AiSVepB7kVlj3V"
+#define pincooling1 4
+#define pinheating1 5
 #define pinbutton_setup 15
 #define pinbutton_up 13
 #define pinbutton_down 12
-#define pinled_on 14
-#define pinled_cool 0
+#define pinled_cool 14
+#define pinled_on 0
 #define pinled_heat 3
 #define ONE_WIRE_BUS 2
 
 // constants
 
+const char* WIFI_SSID =  "ADROIVA";
+const char* WIFI_PASS = "Los5locos";
+
+WiFiClient client;
 
 // variables
 struct switches {
@@ -21,29 +28,29 @@ struct switches {
   bool heating;
   float delta;
   float set;
-  int compressor;
+  unsigned long compressor;
+  long compressor_lag;
   float probe;
 };
 bool button_setup;
 bool button_up;
 bool button_down;
-bool led_on;
-bool led_cool;
-bool led_heat;
-
+unsigned long previousMillis = 0;  // Stores the last time the LED was updated
+const long interval = 1000;  // Interval at which to blink (milliseconds)
+bool blinking;
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-struct switches sw1 = {false, false, 0.5, 10, 3, 0};
-struct switches sw2 = {false, false, 0.5, 10, 3, 0};
+struct switches sw1 = {false, false, 0.5, 40, 0, 10000, 0};
+struct switches sw2 = {false, false, 0.5, 40, 0, 10000, 0};
 
 
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode(pincooling, OUTPUT);
-  pinMode(pinheating, OUTPUT);
+  pinMode(pincooling1, OUTPUT);
+  pinMode(pinheating1, OUTPUT);
   pinMode(pinbutton_setup, INPUT);
   pinMode(pinbutton_up, INPUT);
   pinMode(pinbutton_down, INPUT);
@@ -52,28 +59,105 @@ void setup() {
   pinMode(pinled_heat, OUTPUT);
   sensors.begin();
 
+  Serial.begin(115200);
+  delay(1000);
+  //
+  //  WiFi.mode(WIFI_STA);
+  //  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  //  Serial.print("Conectando a:\t");
+  //  Serial.println(WIFI_SSID);
+  //
+  //  // Esperar a que nos conectemos
+  //  while (WiFi.status() != WL_CONNECTED)
+  //  {
+  //    delay(200);
+  //    Serial.print('.');
+  //  }
+  //
+  //  // Mostrar mensaje de exito y dirección IP asignada
+  //  Serial.println();
+  //  Serial.print("Conectado a:\t");
+  //  Serial.println(WiFi.SSID());
+  //  Serial.print("IP address:\t");
+  //  Serial.println(WiFi.localIP());
+  //
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  sensors.requestTemperatures(); // for the first ubidots status check and data send
-  sw1.probe = sensors.getTempCByIndex(0);
-  sw2.probe = sensors.getTempCByIndex(1);
+  if (Serial.available() > 0) {
+    float value = Serial.parseFloat();
+    if (value != 0) {
+      sw1.set = value;
+    }
+  }
+  sensors.requestTemperatures();
+  sensors.getTempCByIndex(0);
+  //sw1.probe = sensors.getTempCByIndex(0);
+  //sw2.probe = sensors.getTempCByIndex(1);
   temperature_control(sw1);
-  temperature_control(sw2);
-
+  //temperature_control(sw2);
+  relays(sw1, pincooling1, pinheating1);
 }
 
 void temperature_control(switches &sw) {
+  Serial.print("Probe: "); Serial.print(sw.probe);
+  Serial.print(", Set: "); Serial.print(sw.set);
+  Serial.print(", Delta: "); Serial.print(sw.delta);
+  Serial.print(", Heating: "); Serial.print(sw.heating);
+  Serial.print(", Cooling: "); Serial.print(sw.cooling);
+  Serial.print(", mmillis: "); Serial.print(millis());
+  Serial.print(", compressor: "); Serial.println(millis() - sw.compressor);
+
+
   if (!(sw.heating && sw.cooling)) {
-    (sw.probe < (sw.set - sw.delta)) ? sw.heating = true : sw.heating = false;
-    (sw.probe > (sw.set + sw.delta)) ? sw.cooling = true : sw.cooling = false;
+    if (sw.probe < (sw.set - sw.delta)) {
+      sw.heating = true;
+    }
+    if ((sw.probe > (sw.set + sw.delta)) && (millis() - sw.compressor > sw.compressor_lag)) {
+      sw.cooling = true;
+    }
   }
-  if (sw1.heating) {
-    (sw.probe >= sw.set) ? sw.heating = false : sw.heating = true;
+  if (sw.heating) {
+    if (sw.probe >= sw.set) {
+      sw.heating = false;
+    }
   }
-  if (sw1.cooling) {
-    (sw.probe <= sw.set) ? sw.cooling = false : sw.cooling = true;
+  if (sw.cooling) {
+    if (sw.probe <= sw.set) {
+      sw.cooling = false;
+      sw.compressor = millis();
+    }
+  }
+}
+
+void relays(switches &sw, int pincooling, int pinheating) {
+  if (sw.cooling) {
+    digitalWrite(pincooling, HIGH);
+  }
+  else {
+    digitalWrite(pincooling, LOW);
+  }
+  if (sw.heating) {
+    digitalWrite(pinheating, HIGH);
+  }
+  else {
+    digitalWrite(pinheating, LOW);
+  }
+  if ((sw.cooling) && (millis() - sw.compressor < sw.compressor_lag)){
+    nonBlockingBlink(pinled_on, previousMillis, interval);
+  }
+  delay(1000);
+}
+
+void nonBlockingBlink(int ledPin, unsigned long &previousMillis, unsigned long interval) {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    if (digitalRead(ledPin) == LOW) {
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
   }
 }
